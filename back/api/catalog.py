01506c576e20d85fb2ebe085eadc44caa6e1ae33
OblitusNumen <145058017+OblitusNumen@users.noreply.github.com>
2024-11-21 10:22:51 +0300

    Add summary catalog.py

--- back/api/catalog.py
+++ back/api/catalog.py
@@ -18,13 +18,13 @@ class CatalogController(Controller):
     def __init__(self, session: AsyncSession = Depends(get_db_session)):
         self.session = session
 
-    @get("/")
+    @get("/", summary="returns all goods in repository", description="Get all goods")
     async def get_goods(self):
         goods = await GoodRepository(self.session).get_all()
         return list([{"id": good.id, "label": good.name, "price": good.price, "discount": good.discount,
                       "path_to_image": good.img, "in_stock": good.stock} for good in goods])
 
-    @get("/cart")
+    @get("/cart", summary="returns cart for authorized user", description="Get user's cart")
     async def get_cart(self, redis: redis.Redis = Depends(get_redis_client), user: User = Depends(authorize_user)):
         print("a")
         cart = redis.get(f"{RedisDB.cart}:{user.id}")
@@ -33,14 +33,14 @@ class CatalogController(Controller):
             return Cart(contents=[], discount=False)
         return await self.normalize_cart(Cart.model_validate_json(cart.decode('utf-8')))
 
-    @post("/updcart")
+    @post("/updcart", summary="validates and updates cart for authorized user", description="Update cart")
     async def update_cart(self, cart: Cart, redis: redis.Redis = Depends(get_redis_client),
                           user: User = Depends(authorize_user)):
         await self.normalize_cart(cart)
         redis.set(f"{RedisDB.cart}:{user.id}", cart.model_dump_json())
         return {"message": "OK"}
 
-    @get("/getitem")
+    @get("/getitem", summary="returns good with specified id", description="Get good by id")
     async def getGood(self, id: uuid.UUID):
         good = await GoodRepository(self.session).get_by_id(id)
         return good
