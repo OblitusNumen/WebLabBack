fcba6ae750031e79518ae9f40f53091772b324f6
OblitusNumen <o.aleksashkin@yandex.ru>
2024-12-24 15:50:35 +0300

    reformat code

--- proxy/main.js
+++ proxy/main.js
@@ -1,28 +1,28 @@
 var http = require("http"),
-  httpProxy = require("http-proxy");
+    httpProxy = require("http-proxy");
 
 var frontProxy = new httpProxy.createProxyServer({
-  target: {
-    host: "localhost",
-    port: 5173,
-  },
+    target: {
+        host: "localhost",
+        port: 5173,
+    },
 });
 var backProxy = new httpProxy.createProxyServer({
-  target: {
-    host: "localhost",
-    port: 8000,
-  },
+    target: {
+        host: "localhost",
+        port: 8000,
+    },
 });
 var server = http.createServer(function (req, res) {
-  console.log(req.url);
-  if (req.url.startsWith("/api")) {
-    backProxy.web(req, res);
-  } else {
-    frontProxy.web(req, res);
-  }
+    console.log(req.url);
+    if (req.url.startsWith("/api")) {
+        backProxy.web(req, res);
+    } else {
+        frontProxy.web(req, res);
+    }
 });
 server.on("upgrade", function (req, socket, head) {
-  frontProxy.ws(req, socket, head);
+    frontProxy.ws(req, socket, head);
 });
 port = 8081
 console.log(`listening on port ${port}`);
